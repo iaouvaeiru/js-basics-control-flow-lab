6a7ffafb60e2c7e9c4e5175baf15543b7534e0e0
Leon Fan <leonkind@gmail.com>
2021-04-26 16:41:19 -0500

    commit tree 54fe85fa9ec59510aa33225f8953b55d35289766

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  if (rideDistance <= 400) {
+    return 'This one is on me!'
+  } else if (rideDistance >= 2500) {
+    return 'No can do.'
+  } else {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(isNYC) {
+  return (isNYC === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  switch (Tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
